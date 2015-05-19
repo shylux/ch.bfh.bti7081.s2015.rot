3d6acfcc912597d30b3dcc373197fdceb1f02175
James Schüpbach <james.schuepbach@gmail.com>
2015-05-19 22:58:33 +0200

    added files...

---
+++ src/main/java/ch/bfh/bti7081/s2015/red/PatientApp/lifeUp/GpsLocationSimulator.java
@@ -0,0 +1,68 @@
+package ch.bfh.bti7081.s2015.red.PatientApp.lifeUp;
+
+import java.util.ArrayList;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+
+/**
+ * this class simulates a gps location
+ * it provies a path of gps coordinates form a given start point 
+ * to a specific destination. it uses only an approximation (works only for short distances)
+ * 
+ * @author James Mayr
+ *
+ */
+public class GpsLocationSimulator {
+
+	private GpsCoordinate start;
+	private GpsCoordinate end;
+	private GpsCoordinate currentPos;
+	
+	public GpsLocationSimulator(GpsCoordinate start,GpsCoordinate end)
+	{
+		this.start = start;
+		this.end = end;
+	}
+	public ArrayList<GpsCoordinate> path(int steps)
+	{
+		
+		ArrayList<GpsCoordinate>stepList = new ArrayList<>();
+		Vector2D startVector = toVector(start);
+		Vector2D endVector = toVector(end);
+		
+		Vector2D direction = endVector.subtract(startVector);
+		direction = direction.scalarMultiply(1.0/steps);
+		for(int i = 0; i < steps; i++)
+		{
+			startVector = startVector.add(direction);
+			stepList.add(toGpsCoordinate(startVector));
+		}
+		return stepList;
+		
+	}
+	private Vector2D toVector(GpsCoordinate point)
+	{
+		double lat = point.getLatitude();
+		double lng = point.getLongitude();
+		lat =lat/180*Math.PI;
+		lng =lng/180*Math.PI;
+		
+		double xPoint = Math.cos(lng);
+		double yPoint = Math.sin(lat);
+		return new Vector2D(xPoint,yPoint);
+	}
+	private GpsCoordinate toGpsCoordinate(Vector2D position)
+	{
+		double lat = Math.asin(position.getY());
+		double lng = Math.acos(position.getX());
+
+		
+		lat =lat*180/Math.PI;
+		lng = lng*180/Math.PI;
+		
+		return new GpsCoordinate(lat,lng);
+	
+	}
+	
+}
---
+++ src/test/java/ch/bfh/bti7081/s2015/red/PatientApp/Test/TestGpsLocationSimulator.java
@@ -0,0 +1,39 @@
+package ch.bfh.bti7081.s2015.red.PatientApp.Test;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import ch.bfh.bti7081.s2015.red.PatientApp.lifeUp.GpsCoordinate;
+import ch.bfh.bti7081.s2015.red.PatientApp.lifeUp.GpsLocationSimulator;
+
+public class TestGpsLocationSimulator {
+
+	@Test
+	public void TestGpsPaths()
+	{
+		GpsCoordinate start = new GpsCoordinate(46.9644149,7.4563786);
+		GpsCoordinate destination = new GpsCoordinate(46.965032,7.4633031);
+		
+		GpsLocationSimulator simulator = new GpsLocationSimulator(start, destination);
+		
+		int steps = 100;
+		ArrayList<GpsCoordinate> path = simulator.path(steps);
+		
+		
+		Assert.assertEquals(steps, path.size());
+		GpsCoordinate lastPosition = null;
+		for(int i =0; i < steps; i++)
+		{
+			if(i!=0)
+			{
+				lastPosition = path.get(i-1);
+				boolean isNearer = lastPosition.calcDistance(destination) > path.get(i).calcDistance(destination);
+				Assert.assertEquals(true, isNearer);
+			}
+		}
+		Assert.assertEquals(0.0,path.get(steps-1).calcDistance(destination),0.1);
+		
+	}
+}
